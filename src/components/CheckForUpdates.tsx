249b46c692f5a4adf5fda787b2f0b1424e5814f9
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2025-10-08 19:29:27 -0400

    make UI changes

--- src/components/CheckForUpdates.tsx
+++ src/components/CheckForUpdates.tsx
@@ -8,8 +8,8 @@ import { ArrowRightIcon, Loader2Icon } from "lucide-react";
 function AnimatedDot() {
   return (
     <span className="relative flex size-3">
-      <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>{" "}
-      <span className="relative inline-flex size-3 rounded-full bg-red-500"></span>
+      <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-cyan-400 opacity-75"></span>{" "}
+      <span className="relative inline-flex size-3 rounded-full bg-cyan-500"></span>
     </span>
   );
 }
